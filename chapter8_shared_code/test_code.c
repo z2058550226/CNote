a3864cd58016c5735c0000dfe93e3a2f9da3f4ed
z2058550226 <2058550226@qq.com>
2018-07-12 18:26:50 +0800

    chapter 8 hot link libraries

--- chapter8_shared_code/test_code.c
+++ chapter8_shared_code/test_code.c
@@ -3,6 +3,23 @@
 //
 
 #include <stdio.h>
-
 #include "encrypt.h"
 #include "checksum.h"
+
+int main(int argc, char *argv[]) {
+    char s[] = "Speak friend and enter";
+
+    encrypt(s);
+
+    printf("Encrypt to %s\n", s);
+
+    printf("Checksum is %i\n", checksum(s));
+
+    encrypt(s);
+
+    printf("Decrypt back to %s\n", s);
+
+    printf("Checksum is %i\n", checksum(s));
+
+    return 0;
+}
---
+++ lib/hello.h
@@ -0,0 +1,11 @@
+//
+// Created by admin on 2018/7/12.
+//
+
+#ifndef CNOTE_HELLO_H
+#define CNOTE_HELLO_H
+
+#include <stdio.h>
+void hello();
+
+#endif //CNOTE_HELLO_H

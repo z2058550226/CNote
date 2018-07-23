c7b6f0717e32b07dee0e08a73b196fea1e65e2b1
z2058550226 <2058550226@qq.com>
2018-07-23 17:21:17 +0800

    chapter 11 telecommunication

--- chapter11_telecommunication/advice_service.c
+++ chapter11_telecommunication/advice_service.c
@@ -21,24 +21,6 @@ void error(const char *msg) {
     exit(1);
 }
 
-int read_in(int socket, char *buf, int len) {
-    char *s = buf;
-    int slen = len;
-    int c = recv(socket, s, slen, 0);
-    while ((c > 0) && (s[c - 1] != '\n')) {
-        s += c;
-        slen -= c;
-        c = recv(socket, s, slen, 0);
-    }
-    if (c < 0)
-        return c;
-    else if (c == 0)
-        buf[0] = '\0';
-    else
-        s[c - 1] = '\0';
-    return len - slen;
-}
-
 int main(int argc, char *argv[]) {
 
     char *advice[] = {
--- chapter11_telecommunication/ikkp_server.c
+++ chapter11_telecommunication/ikkp_server.c
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <signal.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
 void error(char *msg) {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
@@ -28,7 +31,7 @@ void bind_to_port(int socket, int port) {
     name.sin_family = PF_INET;
     name.sin_port = (in_port_t) htons(30000);
     name.sin_addr.s_addr = htonl(INADDR_ANY);
-    int reuse = 1;
+    int reuse = 1;//重新使用端口，这样重启服务器时就不会出错了
     if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, (char *) &reuse, sizeof(int)) == -1) {
         error("Can't set the reuse option on the socket");
     }
@@ -40,5 +43,100 @@ void bind_to_port(int socket, int port) {
 
 int say(int socket, char *s) {
     int result = send(socket, s, strlen(s), 0);
-
+    if (result == -1) {
+        //这里没有调用error函数，因为服务器不能因为一个客户没连上就关闭。
+        fprintf(stderr, "%s: %s\n", "error occurred when communicate with client", strerror(errno));
+    }
+    return result;
 }
+
+int listener_d;
+
+//关闭程序监听，在关闭程序前关闭套接字。
+void handle_shutdown(int sig) {
+    if (listener_d)
+        close(listener_d);
+
+    fprintf(stderr, "Bye!\n");
+    exit(0);
+}
+
+int read_in(int socket, char *buf, int len) {
+    char *s = buf;
+    int slen = len;
+    int c = recv(socket, s, slen, 0);
+    while ((c > 0) && (s[c - 1] != '\n')) {
+        s += c;
+        slen -= c;
+        c = recv(socket, s, slen, 0);
+    }
+    if (c < 0)
+        return c;
+    else if (c == 0)
+        buf[0] = '\0';
+    else
+        s[c - 1] = '\0';
+    return len - slen;
+}
+
+//监听系统信号
+int catch_signal(int sig, void (*handler)(int)) {
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    return sigaction(sig, &action, NULL);
+}
+
+int main(int argc, char *argv[]) {
+
+    if (catch_signal(SIGINT, handle_shutdown) == -1) {
+        error("Can't set the interrupt handler");
+    }
+
+    listener_d = open_listener_socket();
+    bind_to_port(listener_d, 30000);
+
+    if (listen(listener_d, 10) == -1) {
+        error("can't listen");
+    };
+
+
+    struct sockaddr_storage client_addr;
+    unsigned int address_size = sizeof(client_addr);
+    puts("Waiting for connection");
+    char buf[255];
+    while (1) {
+        int connect_d = accept(listener_d, (struct sockaddr *) &client_addr, &address_size);
+        if (connect_d == -1) {
+            error("can't create secondary socket");
+        }
+
+        if (!fork()) {
+            close(listener_d);
+            if (say(connect_d, "Internet Knock-Knock Protocol Server\r\nVersion 1.0\r\nKnock! Knock!\r\n>") != -1) {
+                read_in(connect_d, buf, sizeof(buf));
+                if (strncasecmp("Who's there?", buf, 12)) {
+                    say(connect_d, "You should say 'Who's there?' !\r\n");
+                } else {
+                    if (say(connect_d, "Oscar\r\n>") != -1) {
+                        read_in(connect_d, buf, sizeof(buf));
+                        if (strncasecmp("Oscar who?", buf, 10)) {
+                            say(connect_d, "You should say 'Oscar who?' !\r\n");
+                        } else {
+                            say(connect_d, "Oscar silly question, you get a silly answer\n");
+                        }
+                    }
+                }
+            }
+            close(connect_d);
+            exit(0);//通信结束以后，子进程应该退出程序。这样就能防止子进程进入服务器的主循环。
+        }
+
+        close(connect_d);
+    }
+
+    return 0;
+}
+
+#pragma clang diagnostic pop
